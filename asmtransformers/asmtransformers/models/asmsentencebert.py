ddee53c8ce167313b40dc6c8c610fc685322f0b6
Ranieri Argentini <1196121+ranieri@users.noreply.github.com>
2026-04-21 13:12:05 +0200

    Refactor ASMSentenceTransformer for length

--- asmtransformers/asmtransformers/models/asmsentencebert.py
+++ asmtransformers/asmtransformers/models/asmsentencebert.py
@@ -58,16 +58,16 @@ class ASMSTTransformer(Transformer):
         # No max_seq_length set. Try to infer from model
         if (
             max_seq_length is None
-            and hasattr(self.auto_model, 'config')
-            and hasattr(self.auto_model.config, 'max_position_embeddings')
+            and hasattr(self.model, 'config')
+            and hasattr(self.model.config, 'max_position_embeddings')
             and hasattr(self.tokenizer, 'model_max_length')
         ):
-            max_seq_length = min(self.auto_model.config.max_position_embeddings, self.tokenizer.model_max_length)
+            max_seq_length = min(self.model.config.max_position_embeddings, self.tokenizer.model_max_length)
 
         self.max_seq_length = max_seq_length
 
-        self.auto_model.tokenizer = tokenizer
-        self.auto_model.config.tokenizer_class = self.tokenizer.__class__.__name__
+        self.model.tokenizer = tokenizer
+        self.model.config.tokenizer_class = self.tokenizer.__class__.__name__
         self.unpad_inputs = False
 
     @property
@@ -77,8 +77,8 @@ class ASMSTTransformer(Transformer):
     @tokenizer.setter
     def tokenizer(self, tokenizer):
         self.processor = tokenizer
-        if hasattr(self, 'auto_model'):
-            self.auto_model.tokenizer = tokenizer
+        if hasattr(self, 'model'):
+            self.model.tokenizer = tokenizer
 
 
 class ASMSentenceTransformer(SentenceTransformer):
@@ -108,50 +108,50 @@ class ASMSentenceTransformer(SentenceTransformer):
      ASMBertModel    ARM64BertTokenizer      T.BertModel    T.BertTokenizer
     """
 
-    def __init__(self, embedding_model, pooling_model, tokenizer):
-        super().__init__(modules=[embedding_model, pooling_model])
-        self.tokenizer = tokenizer
+    def __init__(self, model_name_or_path=None, *, modules=None, **kwargs):
+        super().__init__(model_name_or_path=model_name_or_path, modules=modules, **kwargs)
+
+    @staticmethod
+    def _build_tokenizer(model_name_or_path):
+        tokenizer = ARM64Tokenizer.from_pretrained(model_name_or_path)
+        tokenizer.set_tokenizer(BertTokenizer.from_pretrained(model_name_or_path))
+        return tokenizer
+
+    @classmethod
+    def _build_modules(cls, model_name_or_path, model_args=None):
+        tokenizer = cls._build_tokenizer(model_name_or_path)
+        embedding_model = ASMSTTransformer(model_name_or_path, tokenizer, model_args=model_args or {})
+        pooling_model = Pooling(embedding_model.get_embedding_dimension())
+        return tokenizer, embedding_model, pooling_model
 
     @classmethod
     def from_pretrained(cls, model_name_or_path, model_args=None):
-        tokenizer = ARM64Tokenizer.from_pretrained(model_name_or_path)
-        tokenizer.set_tokenizer(BertTokenizer.from_pretrained(model_name_or_path))
-
-        embedding_model = ASMSTTransformer(model_name_or_path, tokenizer, model_args=model_args or {})
-        pooling_model = Pooling(embedding_model.get_embedding_dimension())
-        return cls(embedding_model, pooling_model, tokenizer)
+        _, embedding_model, pooling_model = cls._build_modules(model_name_or_path, model_args=model_args)
+        return cls(modules=[embedding_model, pooling_model])
 
     @classmethod
     def from_basemodel(cls, base_model_name_or_path, model_args=None):
-        tokenizer = ARM64Tokenizer.from_pretrained(base_model_name_or_path)
-        tokenizer.set_tokenizer(BertTokenizer.from_pretrained(base_model_name_or_path))
-        embedding_model = ASMSTTransformer(base_model_name_or_path, tokenizer, model_args=model_args or {})
-
-        embedding_model.auto_model = ASMBertModel.from_pretrained(base_model_name_or_path)
-        embedding_model.model = embedding_model.auto_model
-        embedding_model.tokenizer = tokenizer
+        _, embedding_model, pooling_model = cls._build_modules(base_model_name_or_path, model_args=model_args)
+        bert_model = embedding_model.model.base_model
 
         # The jTrans architecture shares weights between positional and word embeddings
         # Make sure we have done this properly.
         if (
-            embedding_model.auto_model.base_model.embeddings.position_embeddings
-            is not embedding_model.auto_model.base_model.embeddings.word_embeddings
+            bert_model.embeddings.position_embeddings is not bert_model.embeddings.word_embeddings
         ):
             raise RuntimeError('Word embeddings and position embeddings not shared')
 
         # Now freeze layers, like jTrans. Embedding plus 10 layers is the default, so we'll use that.
-        for param in embedding_model.auto_model.base_model.embeddings.parameters():
+        for param in bert_model.embeddings.parameters():
             param.requires_grad = False
 
         freeze_layer_count = 10
         if freeze_layer_count:
-            for layer in embedding_model.auto_model.base_model.encoder.layer[:freeze_layer_count]:
+            for layer in bert_model.encoder.layer[:freeze_layer_count]:
                 for param in layer.parameters():
                     param.requires_grad = False
 
-        pooling_model = Pooling(embedding_model.get_embedding_dimension())
-
-        return cls(embedding_model, pooling_model, tokenizer)
+        return cls(modules=[embedding_model, pooling_model])
 
     def encode(self, sentences, *args, normalize_embeddings=True, **kwargs):
         # Change the default for normalize_embeddings.
