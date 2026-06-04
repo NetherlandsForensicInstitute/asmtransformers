68652a1fa4e2838e88046ab82171802a53592d55
Lasse Vulto <l.vulto@nfi.nl>
2026-06-04 11:27:49 +0200

    let tokenize_dataset also accept of type datasets.Dataset

--- asmtransformers/scripts/tokenize_dataset.py
+++ asmtransformers/scripts/tokenize_dataset.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     dataset = datasets.load_from_disk(data_in)
 
     # let the tokenizer preprocess data from data_in, write the result to data_out
-    for subset in dataset:
-        dataset[subset] = tokenize(tokenizer, dataset[subset])
+    # for subset in dataset:
+    dataset = tokenize(tokenizer, dataset)
 
     dataset.save_to_disk(data_out)
