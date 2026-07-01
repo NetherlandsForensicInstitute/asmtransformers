21ce7d7fa1f8178b53564729af72ee14c0d7f7a7
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2026-07-01 14:51:05 +0200

    Test auto-creation of schema

--- citatio/tests/test_db_postgresql.py
+++ citatio/tests/test_db_postgresql.py
@@ -11,15 +11,16 @@ pytestmark = pytest.mark.postgresql
 
 
 @contextmanager
-def _local_pgvector():
+def local_pgvector_container():
     with PostgresContainer('pgvector/pgvector:pg18', driver=None) as container:
         yield container
 
 
 @pytest.fixture(scope='session')
-def pgvector():
+def connect_pgvector():
     match environ:
         case {'GITHUB_ACTIONS': _}:
+            # running on GHA, provide connection details to pgvector service defined in .github/workflows/citatio.yml
             yield {
                 'host': environ.get('POSTGRES_HOST', 'postgres'),
                 'port': environ.get('POSTGRES_PORT', 5432),
@@ -28,7 +29,8 @@ def pgvector():
                 'database': environ.get('POSTGRES_DATABASE', 'postgres'),
             }
         case _:
-            with _local_pgvector() as container:
+            # running locally, provide connection details to local pgvector container
+            with local_pgvector_container() as container:
                 yield {
                     'host': container.get_container_host_ip(),
                     'port': container.port,
@@ -38,6 +40,18 @@ def pgvector():
                 }
 
 
-def test_create_schema(pgvector):
-    database = PostgreSQLDatabase(**pgvector)
-    assert database
+@pytest.fixture(scope='session')
+def database(connect_pgvector):
+    return PostgreSQLDatabase(**connect_pgvector)
+
+
+async def test_auto_create_schema_idempotent(connect_pgvector, database):
+    select_tables = 'SELECT table_name FROM information_schema.tables'
+    tables = {name.lower() for name in await database.connection.fetch(select_tables)}
+    expected = {'functions', 'labels'}
+    assert tables & expected == expected
+
+    database2 = PostgreSQLDatabase(**connect_pgvector)
+    assert database != database2
+    assert database.connection != database2.connection
+    assert tables == {name.lower() for name in await database2.connection.fetch(select_tables)}
