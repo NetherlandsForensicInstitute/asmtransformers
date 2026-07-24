13cb541458c375a0e0bb0cd4584269161e9b844b
Anne Fleur van Luenen <a.f.van.luenen@nfi.nl>
2026-07-24 16:09:16 +0200

    ruff

--- asmtransformers/scripts/eval-datasets.py
+++ asmtransformers/scripts/eval-datasets.py
@@ -3,7 +3,7 @@ import random
 
 from datasets import Dataset
 
-from scripts.evaluation import load_test_functions, generate_anchor_pos_pairs, generate_triplets
+from scripts.evaluation import generate_anchor_pos_pairs, generate_triplets, load_test_functions
 
 
 def get_parser() -> argparse.ArgumentParser:
@@ -15,17 +15,17 @@ def get_parser() -> argparse.ArgumentParser:
 
     return parser
 
+
 def main(data_folder, output_folder, pool_size, seed):
     test_functions = load_test_functions(data_folder, architecture=None)
     anchor_rng = random.Random(seed)
     # todo: change num_pairs = 1000 before actually using the code
     anchor_pairs = generate_anchor_pos_pairs(test_functions, anchor_rng, num_pairs=300)
-    test_pools = generate_triplets(
-        test_functions, anchor_pairs, pool_size=pool_size, static_pool=True, rng=seed+1
-    )
+    test_pools = generate_triplets(test_functions, anchor_pairs, pool_size=pool_size, static_pool=True, rng=seed + 1)
     # todo: this does not work! find a way to save the data
     Dataset.from_generator(test_pools)
     Dataset.to_disk(output_folder)
+
 
 if __name__ == '__main__':
     parser = get_parser()
